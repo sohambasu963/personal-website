d9e45ff932c4237ed69ddad98f476b63513c0b8b
Soham Basu <sohambasu963@gmail.com>
2023-12-21 11:20:40 -0500

    Add autoprefixer, postcss, and tailwindcss dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'primary': '#FBF8EF',
+      },
+    },
+  },
+  plugins: [],
+}
+
