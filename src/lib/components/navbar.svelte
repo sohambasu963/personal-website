9fe522dc00a88fd7fce1a4688605648c35fe1c5f
Soham Basu <sohambasu963@gmail.com>
2023-12-25 12:29:44 -0500

    Update navbar spacing

--- src/lib/components/navbar.svelte
+++ src/lib/components/navbar.svelte
@@ -6,11 +6,11 @@
 </script>
 
 <nav class="mt-2">
-  <ul class="flex space-x-4">
+  <ul class="flex space-x-1 md:space-x-4">
     <li class="group">
       <a
         href="/"
-        class="flex items-center space-x-2 py-2 px-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
+        class="flex items-center space-x-1 md:space-x-2 py-2 px-2 pr-8 md:pr-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
       >
         <img src={House} alt="Home" class="w-6 h-6" />
         <span class="text-orange">home</span>
@@ -19,7 +19,7 @@
     <li class="group">
       <a
         href="/about"
-        class="flex items-center space-x-2 py-2 px-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
+        class="flex items-center space-x-1 md:space-x-2 py-2 px-2 pr-8 md:pr-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
       >
         <img src={SohamEmoji} alt="Soham" class="w-6 h-6" />
         <span class="text-orange">about</span>
@@ -28,7 +28,7 @@
     <li class="group">
       <a
         href="/work"
-        class="flex items-center space-x-2 py-2 px-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
+        class="flex items-center space-x-1 md:space-x-2 py-2 px-2 pr-8 md:pr-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
       >
         <img src={WorkingEmoji} alt="Working" class="w-6 h-6" />
         <span class="text-orange">work</span>
@@ -37,7 +37,7 @@
     <li class="group">
       <a
         href="/projects"
-        class="flex items-center space-x-2 py-2 px-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
+        class="flex items-center space-x-1 md:space-x-2 py-2 px-2 pr-8 md:pr-2 group-hover:bg-gray-200 rounded-full transition-colors duration-500 ease-in-out"
       >
         <img src={ProjectsEmoji} alt="Projects" class="w-6 h-6" />
         <span class="text-orange">projects</span>
