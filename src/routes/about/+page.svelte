e779a1d9de26f0497b9c10a483e3529bf8e99f8e
Soham Basu <sohambasu963@gmail.com>
2024-01-25 13:54:45 -0500

    Adjusted about page width

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -60,7 +60,7 @@
   class="min-h-screen flex flex-col items-center md:pb-0 pb-8"
   style="background: linear-gradient(to bottom, #FBF8EF 0%, #F7EFD7 100%);"
 >
-  <div class="flex flex-col items-center" style="width: 90%;">
+  <div class="flex flex-col items-center w-3/4">
     <div class="typewriter mt-4 mt-[7vh] md:mt-[40vh]">
       <h1 class="text-4xl font-tiempos-headline">About</h1>
     </div>
